81f3052f119c9475208d493686d5167d424b119b
murilompo <murilo_mpo@hotmail.com>
2018-01-31 02:18:53 -0200

    Minificação do código
    Código reduzido para o menor número de linhas possíveis.

--- src/main/java/com/github/murilompo/testejava/start/TesteJava.java
+++ src/main/java/com/github/murilompo/testejava/start/TesteJava.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Murilo de Mello P. Oliveira<murilo_mpo@hotmail.com>
@@ -18,29 +19,16 @@ public class TesteJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<Venda> listaVendes = VendasRepostitory.listarTodos();
-
-        Map<Vendedor, Double> vendedorTotalVenda
-                = new HashMap<>(listaVendes.size() / 2);
-
-        listaVendes.forEach((listaVende) -> {
-            Double valor = vendedorTotalVenda.get(listaVende.getVendedor());
-            valor = valor == null
-                    ? listaVende.getValor()
-                    : listaVende.getValor() + valor;
-            vendedorTotalVenda.put(listaVende.getVendedor(), valor);
-        });
-
-        LinkedHashMap<Vendedor, Double> vendedorTotalVendaIndexavel
-                = new LinkedHashMap(vendedorTotalVenda);
 
         System.out.printf("%-15s%-15s\n", "Vendedor", "Total");
-
-        vendedorTotalVendaIndexavel.entrySet().stream()
+        VendasRepostitory.listarTodos()
+                .stream()
+                .collect(Collectors.groupingBy(Venda::getVendedor, Collectors.summingDouble(Venda::getValor)))
+                .entrySet()
+                .stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .forEach(entry -> {
-                    System.out.printf("%-15s%-15.2f\n", entry.getKey().getNome(), entry.getValue());
-                });
+                .forEach(entry -> System.out.printf("%-15s%-15.2f\n", entry.getKey().getNome(), entry.getValue()));
+
     }
 
 }
