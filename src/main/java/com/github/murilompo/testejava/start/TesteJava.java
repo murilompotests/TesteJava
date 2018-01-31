aae8ad2a39ac506aa66ec613705910a686932716
murilompo <murilo_mpo@hotmail.com>
2018-01-31 00:16:51 -0200

    Lógica de processamento
    Utiliza um HashMap para agrupar os vendedores e um LinkedHashMap para ordenar as vendas.

--- src/main/java/com/github/murilompo/testejava/start/TesteJava.java
+++ src/main/java/com/github/murilompo/testejava/start/TesteJava.java
@@ -1,5 +1,14 @@
 package com.github.murilompo.testejava.start;
 
+import com.github.murilompo.testejava.pojo.Venda;
+import com.github.murilompo.testejava.pojo.Vendedor;
+import com.github.murilompo.testejava.repository.VendasRepostitory;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Murilo de Mello P. Oliveira<murilo_mpo@hotmail.com>
  */
@@ -9,7 +18,28 @@ public class TesteJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        List<Venda> listaVendes = VendasRepostitory.listarTodos();
+
+        Map<Vendedor, Double> vendedorTotalVenda
+                = new HashMap<>(listaVendes.size() / 2);
+
+        listaVendes.forEach((listaVende) -> {
+            Double valor = vendedorTotalVenda.get(listaVende.getVendedor());
+            valor = valor == null
+                    ? listaVende.getValor()
+                    : listaVende.getValor() + valor;
+            vendedorTotalVenda.put(listaVende.getVendedor(), valor);
+        });
+
+        LinkedHashMap<Vendedor, Double> vendedorTotalVendaIndexavel
+                = new LinkedHashMap(vendedorTotalVenda);
+
+        vendedorTotalVendaIndexavel.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .forEach(entry -> {
+                    System.out.println(entry.getKey().getNome());
+                    System.out.println(entry.getValue());
+                });
     }
 
 }
